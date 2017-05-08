f234d30cc242638471ec6cd2d0bc88f6c4768496
mightyYaroslav <yaroslavsmth@gmail.com>
2017-05-08 16:32:42 +0300

    Map initialized

---
+++ MetroNavigation/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  MetroNavigation
+//
+//  Created by Yaroslav Bay on 08.05.17.
+//  Copyright © 2017 Yaroslav Bay. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+	@IBOutlet weak var mapView: MKMapView!
+	let regionRadius: CLLocationDistance = 1000
+	let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		centerMapOnLocation(location: initialLocation)
+		addAnnotationToMap()
+	}
+	
+	func centerMapOnLocation(location: CLLocation) {
+		let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+		                                                          regionRadius * 2.0, regionRadius * 2.0)
+		mapView.setRegion(coordinateRegion, animated: true)
+		
+	}
+	
+	func addAnnotationToMap() {
+		let artwork = Artwork(title: "King David Kalakaua",
+		                      locationName: "Waikiki Gateway Park",
+		                      discipline: "Sculpture",
+		                      coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+		
+		mapView.addAnnotation(artwork)
+	}
+	
+	func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+		if let annotation = annotation as? Artwork {
+			let identifier = "pin"
+			var view: MKPinAnnotationView
+			if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+				as? MKPinAnnotationView {
+				dequeuedView.annotation = annotation
+				view = dequeuedView
+			} else {
+				view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+				view.canShowCallout = true
+				view.calloutOffset = CGPoint(x: -5, y: 5)
+				view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+			}
+			return view
+		}
+		return nil
+	}
+	
+	
+
+
+}
+
